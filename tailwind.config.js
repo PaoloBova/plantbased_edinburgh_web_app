7496875abcc405b46682523a23c300a0b184f61f
Paolo Bova <paolobova@protonmail.com>
2025-05-10 14:52:25 +0100

    Improve tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 export default {
-    content: ["./site/**/*.html", "./site/**/*.jsx"],
-    theme: { extend: {} },
-    plugins: []
-  };
-  
+  content: ["./site/**/*.{html,js,jsx}"],
+  theme: { extend: {} },
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
+};
